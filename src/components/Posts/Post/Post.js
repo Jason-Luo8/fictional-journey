976e14c30a1386874a9e6ac6b73b717e29e2b83f
Jason Luo <jasonluo1902@gmail.com>
2022-12-13 10:53:07 +1100

    like function added and bug fixes

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -14,7 +14,7 @@ import moment from "moment";
 import useStyles from "./styles";
 import { useDispatch } from "react-redux";
 
-import { deletePost } from "../../../actions/posts";
+import { deletePost, likePost } from "../../../actions/posts";
 
 const Post = ({ post, setCurrentId }) => {
   const classes = useStyles();
@@ -52,12 +52,16 @@ const Post = ({ post, setCurrentId }) => {
       </Typography>
 
       <CardContent>
-        <Typography variant="h5" gutterBottom>
+        <Typography variant="body1" color="textSecondary" component="p">
           {post.message}
         </Typography>
       </CardContent>
       <CardActions className={classes.cardActions}>
-        <Button size="small" color="primary" onClick={() => {}}>
+        <Button
+          size="small"
+          color="primary"
+          onClick={() => dispatch(likePost(post._id))}
+        >
           <ThumbUpAltIcon fontSize="small" />
           Like
           {post.likeCount}
